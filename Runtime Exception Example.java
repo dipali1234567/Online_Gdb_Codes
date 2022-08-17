f3292f993670413fd172cd79c0ad7c564a0bbb34
Dipali Gangawane <71959065+dipali1234567@users.noreply.github.com>
2022-08-17 12:22:03 +0530

    Runtime Exception Examples

---
+++ Runtime Exception Example.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		String s="Dipali";
+		String s1=null;
+		
+	System.out.println(s1.equals(s));
+	
+	// Hello World
+// Exception in thread "main" java.lang.NullPointerException
+// 	at Main.main(Main.java:18)
+
+
+int a[]=new int[5];
+System.out.println(a[5]);
+	
+// 	Hello World
+// Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
+// 	at Main.main(Main.java:26)
+
+	System.out.println(100/0);
+	
+// 	Hello World
+// Exception in thread "main" java.lang.ArithmeticException: / by zero
+// 	at Main.main(Main.java:32)
+
+		
+	}
+}
+
+
+
+
